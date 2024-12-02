803041209beca5cf434e7041ae13273e78dca3b3
Fall <62287602+FallYT@users.noreply.github.com>
2024-12-02 21:10:50 +0100

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-// Example of using JS to log a message when the "Shop Now" button is clicked
-document.querySelector('.primary-btn').addEventListener('click', function() {
-    alert('Redirecting to the shop!');
-});
